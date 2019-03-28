d8b5df553f9e6bd68749b85e426efdbf2a81fa3c
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2019-03-28 11:27:50 -0500

    updated package.json and snapshots

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,88 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({ adapter: new Adapter() });
+import { shallow } from 'enzyme';
+import App from './App';
+import Card from './components/Card';
+
+const app = shallow(<App />);
+
+describe('App', () => {
+
+    describe('renders correctly', () => {
+
+        it('it looks the same as snapshot', () => {
+            expect(app).toMatchSnapshot();
+        })
+    });
+
+    describe('renders five <Card/> components ', () => {
+        expect(app.find(Card)).toHaveLength(5)
+    })
+
+    describe('Checking Initial State', () => {
+
+
+        it('initializes the `state` with an empty list of alphaNumerics', () => {
+            expect(app.state().alphaNumerics).toEqual([]);
+        })
+
+        it('initializes the `state` with an empty string for twitterHandle ', () => {
+            expect(app.state().twitterHandle).toEqual('');
+        })
+
+        it('initializes the `state` with an empty string for link ', () => {
+            expect(app.state().link).toEqual('');
+        })
+
+        it('initializes the `state` with  0 for pngCount', () => {
+            expect(app.state().pngCount).toEqual(0);
+        })
+
+
+        it('initializes the `state` with  0 for productCount', () => {
+            expect(app.state().productCount).toEqual(0);
+        })
+    })
+
+
+
+    // describe('when clicking the `add-gift` button', () => {
+
+    //     const id = 1;
+
+    //     beforeEach(() => {
+    //         app.find('.btn-add').simulate('click');
+    //     });
+
+    //     afterEach(() => {
+    //         app.setState({ gifts: [] })
+    //     });
+    //     it('adds a new gift to `state` ', () => {
+    //         expect(app.state().gifts).toEqual([{ id: 1 }])
+    //     });
+
+    //     it('adds a new gift to the rendered list ', () => {
+    //         expect(app.find('.gift-list').children().length).toEqual(1);
+    //     });
+
+    //     it('Creates a Gift component', () => {
+    //         expect(app.find('Gift').exists()).toBe(true);
+    //     });
+
+    //     describe('and the user wants to remove the added gift', () => {
+    //         beforeEach(()=> {
+    //             app.instance().removeGift(id);
+    //         });
+
+    //         it('removes the gift from `state`', () => {
+    //             expect(app.state().gifts).toEqual([]);
+    //         });
+    //     });
+
+    // });
+
+
+})
