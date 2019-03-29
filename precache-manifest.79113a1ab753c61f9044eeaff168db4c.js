685e3faae034d80bb460b013294867d111008d3b
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2019-03-29 12:21:15 -0500

    Updates

---
+++ precache-manifest.79113a1ab753c61f9044eeaff168db4c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfb99bc06bff1837813d",
+    "url": "/banno-activity/static/js/runtime~main.685ad64b.js"
+  },
+  {
+    "revision": "5c0177bde575a637129d",
+    "url": "/banno-activity/static/js/main.3897d3dc.chunk.js"
+  },
+  {
+    "revision": "9d5310fde0bbcb7b80be",
+    "url": "/banno-activity/static/js/2.e75da772.chunk.js"
+  },
+  {
+    "revision": "cc92e650d1cae8b86a2dd09d3f57264e",
+    "url": "/banno-activity/index.html"
+  }
+];
