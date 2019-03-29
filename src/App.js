2aa314b3bf3c995caf9d9ebd5194a7440e60f4a2
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2019-03-29 09:06:45 -0500

    seperated functions to helper folder to simply the testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Container, Jumbotron } from 'react-bootstrap';
 import cheerio from 'cheerio';
 import axios from 'axios';
 import Card from './components/Card';
+import { alphaNumerics_top3, pngCount, financialCount, productCount, findTwitterHandle } from './components/helper';
 
 /**
  * 
@@ -39,8 +40,8 @@ class App extends Component {
    * @memberof App
    */
   getDom = () => {
-
-    axios('http://www.banno.com')
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    axios(proxyurl + 'http://www.banno.com')
       .then(({ data }) => {
 
         this.setState({ data })
@@ -72,37 +73,10 @@ class App extends Component {
     const dom = $('body').html();
 
     // look only for alphanumeric 
-    const res = dom.match(/([a-zA-z]+[\d]+)|([\d]+[a-zA-z]+)/g);
-    const obj = {};
+    const topAlphaNumerics = alphaNumerics_top3(dom)
 
-    // count repetitions of alphanumerics
-    res.forEach(item => {
-      if (obj[item] >= 0) {
-        obj[item] = obj[item] + 1;
+    this.setState({ alphaNumerics: topAlphaNumerics })
 
-      } else {
-        obj[item] = 0;
-      }
-    })
-
-    // new array to hold alphanumerics count
-    let newArr = [];
-
-    for (let temp in obj) {
-      newArr.push({ alpha: temp, count: obj[temp] })
-    }
-
-    // Organized array by count value 
-    var sortedArr = newArr.sort((current, next) => next.count - current.count);
-    console.log('== sorted array ===========')
-
-    let topThree = [sortedArr[0], sortedArr[1], sortedArr[2]]
-    this.setState({ alphaNumerics: topThree })
-    // Log top 3 highest count
-    console.log('#1 ', sortedArr[0])
-    console.log('#2 ', sortedArr[1])
-    console.log('#3 ', sortedArr[3])
-    console.log(this.state.alphaNumerics)
   }
 
   /**
@@ -115,12 +89,7 @@ class App extends Component {
 
     const $ = cheerio.load(this.state.data);
     const dom = $('body').html();
-
-    // .png count
-
-    let pngCount = dom.match(/.png/g);
-    console.log('png count : ', pngCount.length)
-    this.setState({ pngCount })
+    this.setState({ pngCount: pngCount(dom) })
 
   }
 
@@ -134,11 +103,8 @@ class App extends Component {
 
     const $ = cheerio.load(this.state.data);
     const dom = $('body').html();
+    this.setState({ financialIntCount: financialCount(dom) })
 
-
-    let financialIntCount = dom.match(/financial institution/g);
-    this.setState({ financialIntCount: financialIntCount.length })
-    console.log('financial int count : ', financialIntCount.length)
   }
 
   /**
@@ -148,23 +114,7 @@ class App extends Component {
    * @memberof App
    */
   handleProductCount = () => {
-
-    const $ = cheerio.load(this.state.data);
-    // const dom = $('body').html();
-
-    // Array to hold products found on the page
-    const products = [];
-
-    $('#sub-menu').children('li').each((i, item) => {
-
-      products.push($(item).find('span').text().trim());
-
-    })
-
-    let productCount = products.length;
-
-    console.log('Current Products Listed ', products)
-    this.setState({ productCount })
+    this.setState({ productCount: productCount(this.state.data) })
   }
 
   /**
@@ -175,30 +125,7 @@ class App extends Component {
    */
   handleTwitterHandle = () => {
 
-    const $ = cheerio.load(this.state.data);
-    // const dom = $('body').html();
-
-    // Find twitter handle
-    let twitterHandle;
-
-    $('.footer-column').each((i, item) => {
-      let liArr = $(item).find('li');
-      $(liArr).each((i, item) => {
-
-        let linkText = $(item).children('a').html()
-
-        if (linkText === 'Twitter') {
-          console.log('Found it !')
-          twitterHandle = $(item).children('a').attr('href');
-
-        }
-      })
-
-      console.log('== Twitter Handle =========');
-      console.log(twitterHandle);
-      this.setState({ twitterHandle })
-    })
-
+    this.setState({ twitterHandle: findTwitterHandle(this.state.data) })
 
   }
 
---
+++ src/components/helper/index.js
@@ -0,0 +1,76 @@
+import cheerio from 'cheerio';
+
+export const alphaNumerics_top3 = dom => {
+
+    const res = dom.match(/([a-zA-z]+[\d]+)|([\d]+[a-zA-z]+)/g);
+    const obj = {};
+
+    // count repetitions of alphanumerics
+    res.forEach(item => {
+        if (obj[item] >= 0) {
+            obj[item] = obj[item] + 1;
+
+        } else {
+            obj[item] = 0;
+        }
+    })
+
+    // new array to hold alphanumerics count
+    let newArr = [];
+
+    for (let temp in obj) {
+        newArr.push({ alpha: temp, count: obj[temp] })
+    }
+
+    // Organized array by count value 
+    var sortedArr = newArr.sort((current, next) => next.count - current.count);
+    console.log('== sorted array ===========')
+
+    let topThree = [sortedArr[0], sortedArr[1], sortedArr[2]]
+
+    return topThree;
+}
+
+export const pngCount = dom => {
+    return dom.match(/.png/g);
+}
+
+export const financialCount = dom => {
+    return dom.match(/financial institution/g).length;
+}
+
+export const productCount = dom => {
+    const $ = cheerio.load(dom);
+
+    // Array to hold products found on the page
+    const products = [];
+
+    $('#sub-menu').children('li').each((i, item) => {
+
+        products.push($(item).find('span').text().trim());
+
+    })
+
+    return products.length;
+}
+
+export const findTwitterHandle = dom => {
+    const $ = cheerio.load(dom);
+
+    let twitterHandle;
+
+    $('.footer-column').each((i, item) => {
+        let liArr = $(item).find('li');
+        $(liArr).each((i, item) => {
+
+            let linkText = $(item).children('a').html()
+
+            if (linkText === 'Twitter') {
+                console.log('Found it !')
+                twitterHandle = $(item).children('a').attr('href');
+
+            }
+        })
+    });
+    return twitterHandle;
+}
